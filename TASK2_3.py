aa9b2dc655d070416e29f2dfea26d0ce5e8ed3d1
tkachenko <tkachenko.ey@rnd.eksmo.ru>
2023-08-08 22:12:32 +0300

    HW commit

---
+++ TASK2_3.py
@@ -0,0 +1,8 @@
+# TASK2_3
+# Требуется вывести все целые степени двойки 
+# (т.е. числа вида 2^k), не превосходящие числа N.
+number = 10
+counter = 1
+while counter <= number:
+    print(counter, end= " ")
+    counter *= 2
